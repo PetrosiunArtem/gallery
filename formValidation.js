c5d4f5398c4766e6cbe4b9f41253d319cde646ee
PetrosiunArtem <petrosiun.av@phystech.edu>
2025-05-12 14:17:52 +0300

    update gallery

--- formValidation.js
+++ formValidation.js
@@ -12,16 +12,95 @@ openPopupButtons.forEach((button) => {
     })
 });
 
-closePopupButton.addEventListener('click',() => {
+closePopupButton.addEventListener('click', () => {
     popupBg.classList.remove('active');
     popup.classList.remove('active');
 });
 
 document.addEventListener('click', (e) => {
-    if(e.target === popupBg) {
+    if (e.target === popupBg) {
         popupBg.classList.remove('active');
         popup.classList.remove('active');
     }
 });
 
-// доде
+function validTelephoneInput() {
+    const phoneRegex = /^7\d{10}$/;
+    if (!phoneRegex.test(myTelephoneInput.value)) {
+        myTelephoneInput.className = 'invalid';
+        telephoneError.innerHTML = 'Номер начинается с семерки и еще после должно быть ровно 10 цифр!';
+    } else {
+        myTelephoneInput.className = 'valid';
+        telephoneError.className = 'show show-out';
+    }
+    validForm();
+}
+
+function validEmailInput() {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(myEmailInput.value)) {
+        myEmailInput.className = 'invalid';
+        emailError.innerHTML = 'Почта должна быть как в примере: Artem1234@mail.ru';
+    } else {
+        myEmailInput.className = 'valid';
+        emailError.innerHTML = '';
+    }
+    validForm();
+}
+
+function validMessageInput() {
+    const pattern = /^[А-Яа-яЁё\s]+$/;
+    if (!pattern.test(myMessageInput.value)) {
+        myMessageInput.className = 'invalid';
+        textError.innerHTML = 'Можно использовать только кириллицу!';
+    } else {
+        myMessageInput.className = 'valid';
+        textError.innerHTML = '';
+    }
+    validForm();
+}
+
+function validForm() {
+
+    mySendButton.disabled = !((myTelephoneInput.classList.contains('valid')) && (myEmailInput.classList.contains('valid')) && (myMessageInput.classList.contains('valid')));
+}
+
+function mySendMessageForm(event) {
+    event.preventDefault();
+    mySendButton.textContent = "Отправляем сообщение";
+    setTimeout(() => {
+        const formData = new FormData(myForm);
+        fetch(myForm.action, {
+            method: 'POST', body: formData
+        }).then((response) => {
+            if (response.ok) {
+                setTimeout(() => {
+                    mySendButton.textContent = "Мы приняли ваш отзыв, спасибо!";
+                }, 2000);
+                setTimeout(() => {
+                    myForm.reset();
+                    mySendButton.textContent = "Отправить";
+                }, 4000)
+                mySendButton.disabled = true;
+            } else {
+                alert("Что-то пошло не так")
+            }
+        })
+    }, 1000);
+}
+
+const myEmailInput = document.getElementById('email');
+const myTelephoneInput = document.getElementById('tel');
+const myMessageInput = document.getElementById('message');
+const mySendButton = document.getElementById('send-button');
+const myForm = document.getElementById('form');
+
+const telephoneError = document.getElementById('telError');
+const emailError = document.getElementById('emailError');
+const textError = document.getElementById('textError');
+
+myTelephoneInput.addEventListener('input', validTelephoneInput);
+myEmailInput.addEventListener('input', validEmailInput);
+myMessageInput.addEventListener('input', validMessageInput);
+
+myForm.addEventListener('submit', mySendMessageForm);
